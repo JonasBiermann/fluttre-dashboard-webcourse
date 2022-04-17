43b5b75d825f703bfec58c3b67c78db35dafc1cb
jonassurface <biermannjonasjacob@gmail.com>
2022-04-17 23:42:36 +0200

    managed to finish appbar

--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_dashboard_skillshare/widgets/responsive_widget.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 28),
+                  child: Image.asset(
+                    'icons/logo.png',
+                    width: 15,
+                  ),
+                ),
+              ],
+            )
+          : IconButton(
+              icon: const Icon(Icons.menu),
+              color: Colors.blue,
+              onPressed: () {
+                key.currentState?.openDrawer();
+              },
+            ),
       elevation: 0,
       backgroundColor: Colors.white,
     );
